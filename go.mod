a5e79f0f7bdd71ae8c8d54eda099a636a29704bb
Jake Levirne <git@jakelevirne.com>
2024-06-01 21:24:20 -0400

    Initial chat controller for OpenAI

--- go.mod
+++ go.mod
@@ -101,7 +101,6 @@ require (
 	github.com/golang/geo v0.0.0-20190812012225-f41920e961ce // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 // indirect
-	github.com/google/subcommands v1.0.1 // indirect
 	github.com/gorilla/css v1.0.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/imdario/mergo v0.3.16 // indirect
@@ -129,6 +128,7 @@ require (
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
+	github.com/sashabaranov/go-openai v1.24.1
 	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/spf13/afero v1.9.2 // indirect
 	github.com/spf13/cast v1.5.0 // indirect
--- internal/controller/chat_controller.go
+++ internal/controller/chat_controller.go
@@ -1,8 +1,12 @@
 package controller
 
 import (
-	"github.com/apache/incubator-answer/internal/base/handler"
+	"io"
+	"net/http"
+	"strings"
+
 	"github.com/gin-gonic/gin"
+	"github.com/sashabaranov/go-openai"
 )
 
 // ChatController chat controller
@@ -22,9 +26,37 @@ func NewChatController() *ChatController {
 // @Router /answer/api/v1/chat/completion [get]
 // @Success 200 {object} map[string]interface{}
 func (cc *ChatController) ChatCompletion(ctx *gin.Context) {
-	handler.HandleResponse(ctx, nil, gin.H{
-		"data": gin.H{
-			"foo": "baz",
+	const MARKDOWN_PROMPT = "hello there"
+	const NEWLINE = "$NEWLINE$"
+	const OPENAI_API_KEY = "<API_KEY>"
+
+	client := openai.NewClient(OPENAI_API_KEY)
+	stream, err := client.CreateChatCompletionStream(ctx, openai.ChatCompletionRequest{
+		Model: "gpt-3.5-turbo",
+		Messages: []openai.ChatCompletionMessage{
+			{Role: "system", Content: "You are a helpful assistant."},
+			{Role: "user", Content: MARKDOWN_PROMPT},
 		},
+		Stream: true,
+	})
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	defer stream.Close()
+
+	ctx.Stream(func(w io.Writer) bool {
+		for {
+			response, err := stream.Recv()
+			if err == io.EOF {
+				return false
+			}
+			if err != nil {
+				ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+				return false
+			}
+			content := strings.ReplaceAll(response.Choices[0].Delta.Content, "\n", NEWLINE)
+			ctx.SSEvent("token", content)
+		}
 	})
 }
